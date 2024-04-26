6aa465811e176c38ba2c9ae1f348a1ad06509ba0
Kostyantyn Zagorodniy <kostyantyn.zagorodniy@gmail.com>
2024-04-27 00:33:49 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,15 @@ echo "::notice file=entrypoint.sh,line=7::$GREETING"
 # Write outputs to the $GITHUB_OUTPUT file
 echo "time=$(date)" >>"$GITHUB_OUTPUT"
 
+echo "starting to download the dummy image: https://placehold.co/600x400/orange/olive/png"
+curl -s -S -o "file.png" https://placehold.co/600x400/orange/olive/png
+
+echo "current directory: $(pwd)"
+echo "files in directory: $(ls -la)"
+
+echo "move files to /github/workspace"
+mv file.png /github/workspace
+
+echo "/github/workspace: $(ls -la /github/workspace)"
+
 exit 0
